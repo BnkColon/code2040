39506a81d9abde9bf252f5062f4b65ff4109000e
Bianca I. Colón Rosado <colon.bianca@gmail.com>
2016-10-01 12:13:06 -0400

    Update 4prefix
    Now I'm using request module and json.

--- 4prefix.py
+++ 4prefix.py
@@ -2,41 +2,55 @@
 
 # Step IV: Prefix
 
-import urllib
+import requests # http://docs.python-requests.org/en/master/user/quickstart/
+import json
+import loggin
 import ast
 import sys
 
+# http://docs.python-requests.org/en/master/api/
+# Enabling debugging at http.client level (requests->urllib3->http.client)
+# you will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# the only thing missing will be the response.body which is not logged.
+try: # for Python 3
+    from http.client import HTTPConnection
+except ImportError:
+    from httplib import HTTPConnection
+HTTPConnection.debuglevel = 1
+
+logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 token = sys.argv[1]
 
-# Get the dictionary
+# Get the dictionary for the challenge
 urlGetDictionary = 'http://challenge.code2040.org/api/prefix'
-data = urllib.urlencode({"token": token})
-conn = urllib.urlopen(urlGetDictionary, data)
-getDictionary = conn.read()
-print 'get' + getDictionary
-dictionary = ast.literal_eval(getDictionary)
+d = {'token':token} # payload
+r = requests.post(urlGetDictionary, json = d) # request
+dictionary = json.loads(r.text)
+print type(dictionary)
 
-# Create new array 
+# Create new array
 prefix = dictionary['prefix']
 print prefix
 array = dictionary['array']
 print array
 newArray = [] 
 for k in array:
-	if prefix  == k[0:len(prefix)]:
+	if k.startswith(prefix):
 		continue
 	else:
 		newArray.append(k)
 print newArray	
 
-# Send
+# Send the response to validate
 urlValidation= 'http://challenge.code2040.org/api/prefix/validate'
 d = {"token": token, "array": newArray}
 print d
-data2 = urllib.urlencode(d)
-print data2
-conn2 = urllib.urlopen(urlValidation, data2)
-response = conn2.read()
+r = requests.post(urlValidation, json = d) # request
+response = r.text
 print response
 
