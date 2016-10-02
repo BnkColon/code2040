370cfbfdb2d0fda788f507879a8a11c28e7f2779
Bianca I. Colón Rosado <colon.bianca@gmail.com>
2016-10-01 23:43:38 -0400

    Add some comments

--- 2reverse.py
+++ 2reverse.py
@@ -8,7 +8,7 @@ import sys
 
 token = sys.argv[1]
 
-## 
+##  Receive the dictionary 
 urlGetWord = "http://challenge.code2040.org/api/reverse"
 data = urllib.urlencode({"token": token})
 conn = urllib.urlopen(urlGetWord, data)
@@ -16,10 +16,12 @@ getWord = conn.read()
 print type(getWord)
 print getWord
 
-## 
+## URL to validate
 urlValidateW = "http://challenge.code2040.org/api/reverse/validate"
+## Reverse the string
 reverse = getWord[::-1]
 print reverse
+# Create dictionary and send
 data2 = urllib.urlencode({"token": token, "string": reverse})
 conn2 = urllib.urlopen(urlValidateW, data2)
 response = conn2.read()
